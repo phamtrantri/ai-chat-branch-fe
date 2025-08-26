09a8b8fcd0824b9b96100c967e1533769abca56b
phamtrantri <phamtrantri@gmail.com>
2025-08-26 22:17:34 +0800

    add resizable menu

--- components/menu.tsx
+++ components/menu.tsx
@@ -12,14 +12,20 @@ const Menu: React.FC<IProps> = ({ conversations }) => {
   const router = useRouter();
   const { id } = router.query || {};
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(true);
   const handleScroll = (e: React.UIEvent<HTMLElement>) => {
     const scrollTop = e.currentTarget.scrollTop;
 
     setIsScrolled(scrollTop > 0);
   };
 
+  const actionClassNames =
+    "flex items-center text-sm mx-1.5 py-1.5 px-2.5 min-h-9 rounded-[10px] hover:bg-gray-200";
+
   return (
-    <div className="relative h-full shrink-0 overflow-hidden border-e border-gray-200 w-[260px] bg-gray-50">
+    <div
+      className={`relative h-full shrink-0 overflow-hidden border-e border-gray-200 bg-gray-50 transition-width duration-200 ease-in-out ${isExpanded ? "w-[260px]" : "w-13"}`}
+    >
       <div className="relative flex h-full flex-col">
         <nav
           className="relative flex h-full w-full flex-1 flex-col overflow-y-auto transition-opacity duration-500"
@@ -32,22 +38,41 @@ const Menu: React.FC<IProps> = ({ conversations }) => {
           >
             <div className="touch:px-1.5 px-2">
               <div
-                className="flex items-center justify-between"
-                style={{ height: "calc(.25rem*13)" }}
+                className={`flex items-center ${isExpanded ? "justify-between" : "justify-center"} h-13`}
               >
-                <div className="pl-1 hover:bg-gray-200 rounded-lg p-1 transition-all duration-200">
-                  <Link href="/">
-                    <Image
-                      alt="Picture of the Chat bot"
-                      height={30}
-                      src="/avatar.svg"
-                      width={30}
-                    />
-                  </Link>
+                <div className="pl-1 rounded-lg p-1 transition-all duration-200">
+                  {isExpanded ? (
+                    <Link href="/" style={{ opacity: isExpanded ? 1 : 0 }}>
+                      <Image
+                        alt="Picture of the Chat bot"
+                        height={30}
+                        src="/avatar.svg"
+                        width={30}
+                      />
+                    </Link>
+                  ) : (
+                    <button
+                      className="cursor-e-resize"
+                      type="button"
+                      onClick={() => {
+                        setIsExpanded(true);
+                      }}
+                    >
+                      <CiMenuFries className="w-4.5 h-4.5" />
+                    </button>
+                  )}
                 </div>
-                <div className="hover:bg-gray-200 p-2 rounded-lg cursor-pointer">
-                  <CiMenuFries className="w-5 h-5" />
-                </div>
+                {isExpanded ? (
+                  <button
+                    className="p-2 rounded-lg cursor-w-resize"
+                    type="button"
+                    onClick={() => {
+                      setIsExpanded(false);
+                    }}
+                  >
+                    <CiMenuFries className="w-4.5 h-4.5" />
+                  </button>
+                ) : null}
               </div>
             </div>
           </div>
@@ -57,42 +82,42 @@ const Menu: React.FC<IProps> = ({ conversations }) => {
             }`}
           >
             <aside className="py-2 flex flex-col">
-              <a
-                className="flex items-center text-sm mx-1.5 min-h-9 py-1.5 px-2.5 rounded-[10px] hover:bg-gray-200"
-                href="/"
-              >
+              <a className={actionClassNames} href="/">
                 <div className="flex items-center gap-2 min-w-0 flex-1">
-                  <CiChat2 className="w-5 h-5 flex-shrink-0" />
-                  <span className="truncate">New Chat</span>
+                  <CiChat2 className="w-4.5 h-4.5 flex-shrink-0" />
+                  {isExpanded ? (
+                    <span className="truncate">New Chat</span>
+                  ) : null}
                 </div>
               </a>
-              <a
-                className="flex items-center text-sm mx-1.5 min-h-9 py-1.5 px-2.5 rounded-[10px] hover:bg-gray-200"
-                href="/"
-              >
+              <a className={actionClassNames} href="/">
                 <div className="flex items-center gap-2 min-w-0 flex-1">
-                  <CiSearch className="w-5 h-5 flex-shrink-0" />
-                  <span className="truncate">Search Chat</span>
+                  <CiSearch className="w-4.5 h-4.5 flex-shrink-0" />
+                  {isExpanded ? (
+                    <span className="truncate">Search Chat</span>
+                  ) : null}
                 </div>
               </a>
             </aside>
           </div>
-          <aside className="flex flex-col py-2.5">
-            <h2 className="block text-sm text-[#8f8f8f] font-normal mx-1.5 my-0 py-2 px-2.5 truncate">
-              Chats
-            </h2>
-            {conversations.map((conv) => (
-              <Link
-                key={conv.id}
-                className={`flex items-center text-sm mx-1.5 min-h-9 py-1.5 
+          {isExpanded ? (
+            <aside className="flex flex-col py-2.5">
+              <h2 className="block text-sm text-[#8f8f8f] font-normal mx-1.5 my-0 py-2 px-2.5 truncate">
+                Chats
+              </h2>
+              {conversations.map((conv) => (
+                <Link
+                  key={conv.id}
+                  className={`flex items-center text-sm mx-1.5 min-h-9 py-1.5 
                   px-2.5 rounded-[10px] hover:bg-gray-200 active:opacity-70 
                   ${Number(id) === conv.id ? "bg-gray-200" : ""}`}
-                href={`/chat/${conv.id}`}
-              >
-                <span className="truncate">{conv.name}</span>
-              </Link>
-            ))}
-          </aside>
+                  href={`/chat/${conv.id}`}
+                >
+                  <span className="truncate">{conv.name}</span>
+                </Link>
+              ))}
+            </aside>
+          ) : null}
         </nav>
       </div>
     </div>
